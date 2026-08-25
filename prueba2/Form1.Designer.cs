ec90f05e0b548424fd91bd6d88d1f01ff1a6bc64
ADSO <ADSO@DESKTOP-HO8RTCE>
2026-08-25 10:04:04 -0500

    Agregar archivos de proyecto.

---
+++ prueba2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace prueba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSi = new System.Windows.Forms.CheckBox();
+            this.btnNo = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(338, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "prueba";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSi
+            // 
+            this.btnSi.AutoSize = true;
+            this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSi.Location = new System.Drawing.Point(261, 118);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(42, 22);
+            this.btnSi.TabIndex = 1;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.CheckedChanged += new System.EventHandler(this.btnSi_CheckedChanged);
+            // 
+            // btnNo
+            // 
+            this.btnNo.AutoSize = true;
+            this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNo.Location = new System.Drawing.Point(396, 118);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(47, 22);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.CheckedChanged += new System.EventHandler(this.btnNo_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(216, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "tu papas saben que eres Gay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox btnSi;
+        private System.Windows.Forms.CheckBox btnNo;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ prueba2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNo_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                // Cambia el texto del botón al acercar el cursor
+                btnNo.Text = "Debes decirle";
+
+                // Genera posiciones aleatorias para que se mueva
+                Random rand = new Random();
+
+                int maxX = this.ClientSize.Width - btnNo.Width;
+                int maxY = this.ClientSize.Height - btnNo.Height;
+
+                int newX = rand.Next(0, maxX);
+                int newY = rand.Next(0, maxY);
+
+                // Mueve el botón a la nueva posición
+                btnNo.Location = new System.Drawing.Point(newX, newY);
+            }
+        }
+
+        private void btnSi_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                MessageBox.Show("¡Sabía que dirías que sí!", "Confirmación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
